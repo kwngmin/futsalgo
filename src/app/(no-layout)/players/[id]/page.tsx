867b3bb0b64e6fcbc9cc5b2453b634edf39a7956
kwngmin <kwngmink@gmail.com>
2025-06-13 23:47:18 +0900

    bugfix

--- src/app/(no-layout)/players/[id]/page.tsx
+++ src/app/(no-layout)/players/[id]/page.tsx
@@ -3,6 +3,10 @@ import PlayerContent from "./ui/PlayerContent";
 const PlayerPage = ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
+  if (!id) {
+    return <div>선수를 찾을 수 없습니다.</div>;
+  }
+
   return <PlayerContent id={id} />;
 };
 
