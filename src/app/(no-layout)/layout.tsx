17f7367da7a80db4b87ba78f977dc2ef975d8929
kwngmin <kwngmink@gmail.com>
2025-06-13 14:00:50 +0900

    nolayout 추가

--- src/app/(no-layout)/layout.tsx
+++ src/app/(no-layout)/layout.tsx
@@ -6,7 +6,7 @@ interface MainLayoutProps {
 
 const NoLayout = async ({ children }: MainLayoutProps) => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-dvh bg-gray-50">
       {/* 데스크톱 & 태블릿 사이드바 */}
       <SideNav />
 
