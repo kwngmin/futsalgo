7c69d58b1af100acd1f5d09d530af81f0d89c56c
kwngmin <kwngmink@gmail.com>
2025-06-23 14:53:02 +0900

    bugfix

--- src/app/(main-layout)/teams/[id]/ui/TeamPlayers.tsx
+++ src/app/(main-layout)/teams/[id]/ui/TeamPlayers.tsx
@@ -139,7 +139,9 @@ const TeamPlayers = ({
                 <ChevronRight className={`w-5 h-5 text-gray-400}`} />
               </button>
               <div className="mx-3 p-1 grid grid-cols-2 gap-2">
-                <Button className="font-semibold">승인</Button>
+                <Button variant="outline" className="font-semibold">
+                  승인
+                </Button>
                 <Button variant="secondary">거절</Button>
               </div>
             </div>
