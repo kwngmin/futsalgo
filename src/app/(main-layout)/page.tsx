d6291933ef0e28d14e5eac8c8ba130f6e1a306f8
kwngmin <kwngmink@gmail.com>
2025-07-14 13:02:44 +0900

    소소한 수정

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -38,7 +38,7 @@ const HomePage = () => {
       <div className="flex items-center justify-between px-4 h-16 shrink-0">
         <div className="flex gap-3">
           <h1 className="text-2xl font-bold">경기</h1>
-          <h1 className="text-2xl font-bold opacity-30">지난 경기</h1>
+          <h1 className="text-2xl font-bold opacity-30">북마킹</h1>
         </div>
         <div className="flex items-center gap-2">
           <button className="shrink-0 size-10 flex items-center justify-center text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-full transition-colors cursor-pointer">
@@ -104,7 +104,7 @@ const HomePage = () => {
                 </span>
               </div>
 
-              <div className="flex justify-between px-4 gap-3">
+              <div className="h-11 flex items-center justify-between px-4 gap-3">
                 <div className="text-lg font-semibold">{schedule.place}</div>
                 <span className="text-muted-foreground">
                   {schedule.matchType === "TEAM" ? "외부팀 대전" : "자체전"}
