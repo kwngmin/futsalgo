7fff0b5e3f14fa61813a216c2715388948243bb3
kwngmin <kwngmink@gmail.com>
2025-07-23 22:36:23 +0900

    bugfix

--- src/app/(main-layout)/ui/ScheduleCard.tsx
+++ src/app/(main-layout)/ui/ScheduleCard.tsx
@@ -102,13 +102,13 @@ const ScheduleCard = ({ schedule }: { schedule: ScheduleCardProps }) => {
         >
           <HeartIcon
             className={`size-6 group-hover:animate-ping absolute ${
-              isLiked ? "text-indigo-500" : "text-zinc-300"
+              isLiked ? "text-indigo-600" : "text-zinc-300"
             }`}
             weight="fill"
           />
           <HeartIcon
             className={`size-6 group-hover: ${
-              isLiked ? "text-indigo-500" : "text-zinc-300"
+              isLiked ? "text-indigo-600" : "text-zinc-300"
             }`}
             weight="fill"
           />
