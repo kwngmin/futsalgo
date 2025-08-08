8f38fdb5bf50aa67f69f333f93cd20a7999adb27
kwngmin <kwngmink@gmail.com>
2025-08-08 17:18:15 +0900

    댓글 ui 수정

--- src/app/(main-layout)/schedule/[id]/ui/ScheduleComments.tsx
+++ src/app/(main-layout)/schedule/[id]/ui/ScheduleComments.tsx
@@ -2,14 +2,7 @@
 
 import React, { useState, useOptimistic, useTransition } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import {
-  MessageCircle,
-  //   Reply,
-  User,
-  Loader2,
-  AlertCircle,
-  //   MessageCircleMore,
-} from "lucide-react";
+import { MessageCircle, User, Loader2, AlertCircle } from "lucide-react";
 import {
   addComment,
   getScheduleComments,
@@ -369,6 +362,7 @@ const ScheduleComments: React.FC<ScheduleCommentsProps> = ({ scheduleId }) => {
 
               {!isReply && !isOptimistic && (
                 <div className="flex items-center gap-1 mt-2">
+                  <MessageCircle className="size-4 text-gray-500" />
                   {comment.replies.length > 0 && (
                     <span className="text-sm font-medium text-gray-500">
                       답글 {comment.replies.length}개 •
@@ -378,7 +372,7 @@ const ScheduleComments: React.FC<ScheduleCommentsProps> = ({ scheduleId }) => {
                     <button
                       onClick={() => setReplyingTo(comment.id)}
                       disabled={addCommentMutation.isPending}
-                      className="sm:text-sm text-blue-700 hover:text-blue-800 flex items-center justify-center disabled:opacity-50 cursor-pointer font-semibold"
+                      className="sm:text-sm hover:underline underline-offset-2 flex items-center justify-center disabled:opacity-50 cursor-pointer font-semibold"
                     >
                       답글 추가
                     </button>
