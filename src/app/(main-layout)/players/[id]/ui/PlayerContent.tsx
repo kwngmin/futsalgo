e499b35938b507518c3c419bf04e1763d2061453
kwngmin <kwngmink@gmail.com>
2025-08-11 16:52:51 +0900

    회원 ui 수정

--- src/app/(main-layout)/players/[id]/ui/PlayerContent.tsx
+++ src/app/(main-layout)/players/[id]/ui/PlayerContent.tsx
@@ -46,11 +46,6 @@ const tabs = [
     value: "matches",
     isDisabled: true,
   },
-  // {
-  //   label: "후기",
-  //   value: "reviews",
-  //   isDisabled: true,
-  // },
   {
     label: "사진",
     value: "photos",
@@ -60,7 +55,6 @@ const tabs = [
 
 const PlayerContent = ({ id }: { id: string }) => {
   const router = useRouter();
-  // const session = useSession();
 
   const [selectedTab, setSelectedTab] = useState<string>(tabs[0].value);
 
@@ -153,172 +147,33 @@ const PlayerContent = ({ id }: { id: string }) => {
                           }
                         )} 가입`
                       : "데이터 없음"}
-                    {/* • ${data.data. */}
                   </span>
-                  {/* {data.data.teams && data.data.teams.length > 0 ? (
-                  <button
-                    onClick={() =>
-                      router.push(`/teams/${data.data.teams[0].team.id}`)
-                    }
-                    className="w-full flex items-center justify-between gap-1.5 cursor-pointer"
-                  >
-                    <Image
-                      width={24}
-                      height={24}
-                      src={
-                        logoOptions[
-                          Math.floor(Math.random() * logoOptions.length)
-                        ]
-                      }
-                      alt="profile_image"
-                      className="size-6 object-cover rounded-md overflow-hidden"
-                      unoptimized
-                    />
-                    <span className="font-medium shrink-0 text-sm">
-                      {data.data.teams[0].team.name || "팀 이름 없음"}
-                      <span className="font-normal">
-                        {" "}
-                        •{" "}
-                        {
-                          TEAM_GENDER[
-                            data.data.teams[0].team
-                              .gender as keyof typeof TEAM_GENDER
-                          ]
-                        }
-                      </span>
-                    </span>
-                  </button>
-                ) : (
-                  <span className="font-medium shrink-0 text-muted-foreground text-sm">
-                    소속 팀 없음
-                  </span>
-                )} */}
                 </div>
               </div>
               <MannerBar />
-              {/* <MannerBar score={Math.floor(Math.random() * 100)} /> */}
             </div>
-            {/* {data?.data?.teams[0]?.team ? (
-              <TeamCard team={data?.data?.teams[0]?.team} />
-            ) : (
-              <div className="flex items-center gap-2 p-3 h-20 border-t border-gray-100">
-                <div className="size-14 flex items-center justify-center">
-                  <div className="size-10 bg-gradient-to-br from-slate-300 to-gray-100 rounded-full " />
-                </div>
-                <span className="sm:text-sm text-muted-foreground font-medium">
-                  소속 팀 없음
-                </span>
-              </div>
-            )} */}
+
             {/* 탭 */}
             <div className="flex items-center justify-between gap-2 px-4">
               <div className="flex h-12 space-x-2">
-                {tabs
-                  // .filter(
-                  //   (tab) =>
-                  //     tab.value !== "management" ||
-                  //     (tab.value === "management" &&
-                  //       (data.data.currentUserMembership.role === "MANAGER" ||
-                  //         data.data.currentUserMembership.role === "OWNER") &&
-                  //       data.data.currentUserMembership.status === "APPROVED")
-                  // )
-                  .map((tab) => (
-                    <div
-                      key={tab.value}
-                      className={`flex justify-center items-center min-w-14 font-semibold text-base px-2 cursor-pointer border-b-4 ${
-                        selectedTab === tab.value
-                          ? "border-gray-700"
-                          : "border-transparent"
-                      } ${
-                        tab.isDisabled ? "pointer-events-none opacity-50" : ""
-                      }`}
-                      onClick={() => setSelectedTab(tab.value)}
-                    >
-                      {tab.label}
-                      {/* <div className={` rounded-t-full h-0.5 w-full flex overflow-hidden ${selectedTab === tab.value ? "":""}`} /> */}
-                    </div>
-                  ))}
+                {tabs.map((tab) => (
+                  <div
+                    key={tab.value}
+                    className={`flex justify-center items-center min-w-14 font-semibold text-base px-2 cursor-pointer border-b-4 ${
+                      selectedTab === tab.value
+                        ? "border-gray-700"
+                        : "border-transparent"
+                    } ${
+                      tab.isDisabled ? "pointer-events-none opacity-50" : ""
+                    }`}
+                    onClick={() => setSelectedTab(tab.value)}
+                  >
+                    {tab.label}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-
-          {/* 소속 팀 */}
-          {/* {data?.data?.teams[0]?.team ? (
-            <div className="bg-white rounded-2xl overflow-hidden">
-              <TeamCard team={data?.data?.teams[0]?.team} />
-            </div>
-          ) : (
-            <div className="flex items-center gap-2 p-3 bg-white/50 rounded-2xl h-20">
-              <div className="size-10 bg-gradient-to-br from-slate-300 to-gray-100 rounded-full " />
-              <span className="sm:text-sm text-muted-foreground font-medium">
-                소속 팀 없음
-              </span>
-            </div>
-          )} */}
-          {/* {data.data.teams && data.data.teams.length > 0 ? (
-            <div className="bg-white rounded-2xl">
-              <button
-                onClick={() =>
-                  router.push(`/teams/${data.data.teams[0].team.id}`)
-                }
-                className="w-full flex items-center justify-between px-4 py-3 hover:bg-gray-50 transition-colors cursor-pointer"
-              >
-                <div className="flex items-center space-x-3">
-                  <Image
-                    width={24}
-                    height={24}
-                    src={
-                      logoOptions[
-                        Math.floor(Math.random() * logoOptions.length)
-                      ]
-                    }
-                    alt="profile_image"
-                    className="size-6 object-cover rounded-md overflow-hidden"
-                    unoptimized
-                  />
-                  <span className="font-medium shrink-0">
-                    {data.data.teams[0].team.name || "팀 이름 없음"}
-                  </span>
-                </div>
-                <ChevronRight className="size-5 text-gray-400" />
-              </button>
-            </div>
-          ) : (
-            <div className="bg-white rounded-lg overflow-hidden opacity-50 pointer-events-none">
-              <button
-                onClick={() => alert("플레이 정보 통계")}
-                className={`w-full flex items-center justify-between px-4 py-3 hover:bg-gray-50 transition-colors`}
-              >
-                <div className="flex items-center space-x-3">
-                  <div className="size-6 bg-gray-200 rounded-full" />
-                  <span className="font-medium">소속 팀 없음</span>
-                </div>
-                <ChevronRight className="size-5 text-gray-400" />
-              </button>
-            </div>
-          )} */}
-
-          {/* 성별 및 이름 */}
-          {/* <div className="bg-white rounded-lg overflow-hidden">
-            <button
-              className={`w-full flex items-center justify-between px-4 py-3 hover:bg-gray-50 transition-colors`}
-            >
-              <div className="flex items-center space-x-3">
-                <User2 className={`size-5 text-gray-600`} />
-                <span className="font-medium">{`${
-                  data?.data?.gender
-                    ? `${GENDER[data?.data?.gender as keyof typeof GENDER]}`
-                    : "성별 미설정"
-                } • ${
-                  data?.data?.name
-                    ? id === session.data?.user.id
-                      ? data?.data?.name
-                      : maskName(data?.data?.name)
-                    : "이름 없음"
-                }`}</span>
-              </div>
-            </button>
-          </div> */}
 
           {/* 소속 팀 */}
           {selectedTab === "teams" &&
@@ -376,59 +231,6 @@ const PlayerContent = ({ id }: { id: string }) => {
                   <Label className="text-muted-foreground">부상</Label>
                 </div>
               </div>
-              {/* <div className="border rounded-2xl overflow-hidden mx-4">
-                <div className="w-full flex items-center px-4 py-3 border-b bg-gray-50">
-                  <div className="flex items-center space-x-3">
-                    <List className={`size-5 text-gray-600`} />
-                    <span className="font-medium">기본 정보</span>
-                  </div>
-                </div>
-                <div className="grid grid-cols-3 sm:grid-cols-6 gap-3 p-4">
-                  <div className="flex flex-col gap-1 items-center my-3">
-                    <div className="font-semibold">
-                      {GENDER[data?.data?.gender as keyof typeof GENDER]}
-                    </div>
-                    <Label className="text-muted-foreground">성별</Label>
-                  </div>
-                  <div className="flex flex-col gap-1 items-center my-3">
-                    <div className="font-semibold">
-                      {
-                        PLAYER_BACKGROUND[
-                          data?.data
-                            ?.playerBackground as keyof typeof PLAYER_BACKGROUND
-                        ]
-                      }
-                    </div>
-                    <Label className="text-muted-foreground">출신</Label>
-                  </div>
-                  <div className="flex flex-col gap-1 items-center my-3">
-                    <div className="font-semibold">
-                      {getCurrentAge(data?.data?.birthDate as string).age}살
-                    </div>
-                    <Label className="text-muted-foreground">나이</Label>
-                  </div>
-                  <div className="flex flex-col gap-1 items-center my-3">
-                    <div className="font-semibold">{data?.data?.height}cm</div>
-                    <Label className="text-muted-foreground">키</Label>
-                  </div>
-                  <div className="flex flex-col gap-1 items-center my-3">
-                    <div className="font-semibold">
-                      {FOOT[data?.data?.foot as keyof typeof FOOT]}
-                    </div>
-                    <Label className="text-muted-foreground">사용하는 발</Label>
-                  </div>
-                  <div className="flex flex-col gap-1 items-center my-3">
-                    <div className="font-semibold">
-                      {
-                        CONDITION[
-                          data?.data?.condition as keyof typeof CONDITION
-                        ]
-                      }
-                    </div>
-                    <Label className="text-muted-foreground">부상</Label>
-                  </div>
-                </div>
-              </div> */}
 
               {/* 선호 포지션 */}
               <div className="grid grid-cols-5 sm:grid-cols-4 gap-2 mx-4">
@@ -453,13 +255,6 @@ const PlayerContent = ({ id }: { id: string }) => {
                         ]
                       }
                     </span>
-                    {/* <Label className="text-muted-foreground break-keep text-center leading-tight sm:w-4/5 hidden sm:block">
-                  {
-                    POSITION_DESCRIPTION[
-                      data?.data?.position as keyof typeof POSITION_DESCRIPTION
-                    ]
-                  }
-                </Label> */}
                   </div>
                 </div>
                 {/* 피치 포지션 */}
@@ -592,43 +387,6 @@ const PlayerContent = ({ id }: { id: string }) => {
               </div>
             </Fragment>
           )}
-
-          {/* 연습 경기 */}
-          {/* <div className="flex flex-col bg-white rounded-2xl overflow-hidden space-y-3">
-            <button
-              onClick={() => alert("연습 경기")}
-              className="w-full flex items-center justify-between px-4 py-3 hover:bg-gray-50 transition-colors border-b border-gray-100 cursor-pointer"
-            >
-              <div className="flex items-center space-x-3">
-                <Volleyball className={`size-5 text-gray-600`} />
-                <span className="font-medium">2025년</span>
-              </div>
-              <ChevronRight className="size-5 text-gray-400" />
-            </button>
-            <div className="grid grid-cols-4 gap-3 bg-white rounded-2xl p-4">
-              <div className="flex flex-col gap-1 items-center my-4">
-                <div className="font-semibold">2</div>
-                <Label className="text-muted-foreground">경기</Label>
-              </div>
-              <div className="flex flex-col gap-1 items-center my-4">
-                <div className="font-semibold">0</div>
-                <Label className="text-muted-foreground">득점</Label>
-              </div>
-              <div className="flex flex-col gap-1 items-center my-4">
-                <div className="font-semibold">1</div>
-                <Label className="text-muted-foreground">어시스트</Label>
-              </div>
-              <div className="flex flex-col gap-1 items-center my-4">
-                <div className="font-semibold">0</div>
-                <Label className="text-muted-foreground">출전 시간</Label>
-              </div>
-            </div>
-          </div> */}
-          {/* <div className="bg-slate-300">
-            <div className="w-fit p-3">
-              <FutsalPitch className="w-24" fill="gray" />
-            </div>
-          </div> */}
         </div>
       ) : null}
       {/* <p className="text-center text-sm text-gray-500 mt-3">
