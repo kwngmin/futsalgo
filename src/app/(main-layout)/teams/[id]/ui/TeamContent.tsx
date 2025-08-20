85513a6cec170770c3ba4d4dcb4fec5ee71aa14b
kwngmin <kwngmink@gmail.com>
2025-08-20 14:41:39 +0900

    팀상세 ui 수정

--- src/app/(main-layout)/teams/[id]/ui/TeamContent.tsx
+++ src/app/(main-layout)/teams/[id]/ui/TeamContent.tsx
@@ -440,7 +440,7 @@ const TeamContent = ({ id }: { id: string }) => {
           {selectedTab === "overview" && (
             <Fragment>
               {/* 기본 정보 */}
-              <div className="bg-neutral-100 rounded-2xl mx-4 grid grid-cols-3 sm:grid-cols-6 gap-3 p-4">
+              <div className="bg-neutral-50 rounded-2xl mx-4 grid grid-cols-3 sm:grid-cols-6 gap-3 p-4">
                 <div className="flex flex-col gap-1 items-center my-3">
                   <div className="font-semibold">
                     {
