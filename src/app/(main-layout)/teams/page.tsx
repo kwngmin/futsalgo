84b076ca3c0d95332377ee1189bdf19211f1c098
kwngmin <kwngmink@gmail.com>
2025-09-12 10:40:37 +0900

    새로운 일정 추가 및 팀 등록 버튼 ui 수정, 하단 독 높이 변경

--- src/app/(main-layout)/teams/page.tsx
+++ src/app/(main-layout)/teams/page.tsx
@@ -151,7 +151,8 @@ const TeamsPage = () => {
         <button
           type="button"
           onClick={() => router.push(isLoggedIn ? "/teams/create" : "/")}
-          className="fixed bottom-[58px] sm:bottom-16 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-2xs md:max-w-2xl mx-auto shrink-0 h-14 sm:h-10 md:h-11 flex items-center justify-between bg-emerald-600 text-white hover:bg-emerald-800 rounded-t-3xl sm:rounded-full md:rounded-b-none md:rounded-t-2xl transition-colors cursor-pointer font-semibold z-20 px-5 sm:px-2 md:px-3 active:bg-black"
+          // className="fixed bottom-24 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-2xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 sm:h-10 md:h-11 flex items-center justify-between bg-emerald-600 text-white hover:bg-emerald-800 rounded-full md:rounded-b-none md:rounded-t-2xl transition-colors cursor-pointer font-semibold z-20 px-3 sm:px-2.5 md:px-3 active:bg-emerald-900 shadow-md inset-shadow-sm inset-shadow-white/50"
+          className="fixed bottom-24 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-2xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 sm:h-10 md:h-11 flex items-center justify-between bg-indigo-600 text-white hover:bg-indigo-800 rounded-full md:rounded-b-none md:rounded-t-2xl transition-colors cursor-pointer font-semibold z-20 px-3 sm:px-2.5 md:px-3 active:bg-indigo-900 shadow-md inset-shadow-sm inset-shadow-white/50"
         >
           <div className="flex items-center justify-center gap-3 sm:gap-2">
             <div className="shrink-0 size-7 sm:size-6 flex items-center justify-center bg-white text-emerald-700 rounded-full">
--- src/widgets/BottomNav.tsx
+++ src/widgets/BottomNav.tsx
@@ -9,7 +9,7 @@ const BottomNav = () => {
   };
 
   return (
-    <nav className="z-20 fixed bottom-4 mx-4 left-0 right-0 bg-gradient-to-b from-transparent to-white backdrop-blur-lg rounded-xl md:hidden sm:max-w-xl sm:mx-auto px-0.5 sm:px-1.5 inset-shadow-sm inset-shadow-gray-200/50 shadow-md">
+    <nav className="z-20 fixed bottom-4 mx-4 left-0 right-0 bg-gradient-to-b from-transparent to-white backdrop-blur-md rounded-xl md:hidden sm:max-w-xl sm:mx-auto px-0.5 sm:px-1.5 inset-shadow-sm inset-shadow-gray-200/50 shadow-md">
       <div className="flex my-auto">
         {navItems.map((item) => {
           const Icon = item.icon;
@@ -18,7 +18,7 @@ const BottomNav = () => {
             <button
               key={item.id}
               onClick={() => handleClick(item.href)}
-              className={`h-16 flex-1 flex flex-col justify-center items-center py-2 px-1 transition-colors cursor-pointer ${
+              className={`h-18 flex-1 flex flex-col justify-center items-center py-2 px-1 transition-colors cursor-pointer ${
                 // isActive ? "text-gray-700" : "text-neutral-400"
                 // isActive ? "text-blue-600" : "text-gray-700"
                 isActive ? "text-indigo-700" : "text-gray-700"
@@ -26,11 +26,11 @@ const BottomNav = () => {
               type="button"
             >
               <Icon
-                className="mb-0.5"
+                className="my-1"
                 // className="mb-1 mt-0.5"
                 // className="w-6 h-6 mb-1"
                 // weight="fill"
-                size={24}
+                size={28}
                 weight={isActive ? "fill" : "regular"}
               />
               <span className="text-xs font-medium">{item.label}</span>
