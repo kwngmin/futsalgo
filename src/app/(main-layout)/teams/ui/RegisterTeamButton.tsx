52e5fe7eb24f308b80f6e05725ade6003c7b168a
kwngmin <kwngmink@gmail.com>
2025-09-15 17:21:30 +0900

    register button y position 조정

--- src/app/(main-layout)/teams/ui/RegisterTeamButton.tsx
+++ src/app/(main-layout)/teams/ui/RegisterTeamButton.tsx
@@ -12,7 +12,7 @@ const RegisterTeamButton = ({ onClick }: RegisterTeamButtonProps) => {
     <button
       type="button"
       onClick={onClick}
-      className="fixed bottom-24 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 sm:h-11 flex items-center justify-between bg-emerald-600 text-white hover:bg-emerald-800 rounded-full md:rounded-b-none md:rounded-t-2xl cursor-pointer font-semibold z-20 px-3 active:bg-emerald-900 active:scale-98 transition-all shadow-md inset-shadow-sm inset-shadow-white/50"
+      className="fixed bottom-28 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 sm:h-11 flex items-center justify-between bg-emerald-600 text-white hover:bg-emerald-800 rounded-full md:rounded-b-none md:rounded-t-2xl cursor-pointer font-semibold z-20 px-3 active:bg-emerald-900 active:scale-98 transition-all shadow-md inset-shadow-sm inset-shadow-white/50"
     >
       <div className="flex items-center justify-center gap-3 sm:gap-2">
         <div className="shrink-0 size-7 sm:size-6 flex items-center justify-center bg-white text-emerald-700 rounded-full">
