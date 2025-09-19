801c4a6e3094da89584074d1d7c50d62b04a6c3f
kwngmin <kwngmink@gmail.com>
2025-09-19 14:50:19 +0900

    소소한 ui 수정

--- src/app/(no-layout)/schedule/[id]/match/[matchId]/ui/MatchContent.tsx
+++ src/app/(no-layout)/schedule/[id]/match/[matchId]/ui/MatchContent.tsx
@@ -566,7 +566,7 @@ const MatchContent = ({ data }: MatchContentProps) => {
           {mode === "view" ? (
             <div className="grid grid-cols-2 gap-2">
               <div className="flex flex-col bg-gray-50 p-1 rounded-2xl">
-                <div className="flex flex-col sm:flex-row justify-center sm:justify-start sm:items-center gap-x-2 gap-y-0.5 shrink-0 h-14 sm:h-10 *:leading-tight px-4">
+                <div className="flex flex-col sm:flex-row justify-center sm:justify-start sm:items-center gap-x-2 gap-y-0.5 shrink-0 h-14 sm:h-10 *:leading-tight px-3 sm:px-4">
                   <span className="text-sm font-medium text-gray-800">
                     {data.match.schedule.matchType === "SQUAD"
                       ? `A팀`
@@ -586,7 +586,7 @@ const MatchContent = ({ data }: MatchContentProps) => {
                 />
               </div>
               <div className="flex flex-col bg-gray-50 p-1 rounded-2xl">
-                <div className="flex flex-col sm:flex-row justify-center sm:justify-start sm:items-center gap-x-2 gap-y-0.5 shrink-0 h-14 sm:h-10 *:leading-tight px-4">
+                <div className="flex flex-col sm:flex-row justify-center sm:justify-start sm:items-center gap-x-2 gap-y-0.5 shrink-0 h-14 sm:h-10 *:leading-tight px-3 sm:px-4">
                   <span className="text-sm font-medium text-gray-800">
                     {data.match.schedule.matchType === "SQUAD"
                       ? `B팀`
