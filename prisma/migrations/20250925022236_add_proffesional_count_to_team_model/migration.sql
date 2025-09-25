1f463dc5482aa38e680a0307ecda5dae4717fb37
kwngmin <kwngmink@gmail.com>
2025-09-25 13:09:18 +0900

    팀 모델 수정

---
+++ prisma/migrations/20250925022236_add_proffesional_count_to_team_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "teams" ADD COLUMN     "professionalCount" INTEGER NOT NULL DEFAULT 0;
---
+++ prisma/migrations/20250925040705_add_proffesional_count_to_conditional/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "teams" ALTER COLUMN "professionalCount" DROP NOT NULL,
+ALTER COLUMN "professionalCount" DROP DEFAULT;
