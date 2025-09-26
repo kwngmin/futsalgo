86afd079bebd89231fb9c357f5bc7e7df25d84d6
kwngmin <kwngmink@gmail.com>
2025-09-26 22:20:40 +0900

    결기일정,내일정,팀,회원,팔로잉 loading.tsx 추가

---
+++ src/app/(main-layout)/teams/loading.tsx
@@ -0,0 +1,22 @@
+import SkeletonTeamContent from "./ui/SkeletonTeamContent";
+
+const TeamsPageLoading = () => {
+  return (
+    <div className="max-w-2xl mx-auto pb-16 flex flex-col">
+      <div className="flex items-center justify-between px-4 h-16 shrink-0">
+        <div className="flex gap-3">
+          <h1 className="text-[1.625rem] font-bold cursor-default">팀</h1>
+          <h1 className="text-[1.625rem] font-bold cursor-default opacity-30">
+            팔로잉
+          </h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <button className="shrink-0 size-10 flex items-center justify-center bg-gray-100 rounded-full animate-pulse" />
+        </div>
+      </div>
+      <SkeletonTeamContent />
+    </div>
+  );
+};
+
+export default TeamsPageLoading;
