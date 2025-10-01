37059fd0e7351239250358e7dbccf6c4ace8a5a8
kwngmin <kwngmink@gmail.com>
2025-10-01 23:07:35 +0900

    bugfix 개발환경에서 경기일정 렌더링 안되는 이슈

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -3,27 +3,23 @@ import SchedulePageLoading from "./home/ui/loading";
 import { getSchedules } from "./home/actions/get-schedules";
 import SchedulesInfiniteClient from "./home/ui/SchedulesInfiniteClient";
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams: { search?: string };
+export default async function HomePage(props: {
+  searchParams: Promise<{ search?: string }>;
 }) {
+  const searchParams = await props.searchParams;
+
   return (
     <div className="max-w-2xl mx-auto pb-16 flex flex-col">
       <Suspense fallback={<SchedulePageLoading isPage />}>
-        <ScheduleDataFetcher searchParams={searchParams} />
+        <ScheduleDataFetcher searchQuery={searchParams.search} />
       </Suspense>
     </div>
   );
 }
 
-async function ScheduleDataFetcher({
-  searchParams,
-}: {
-  searchParams: { search?: string };
-}) {
+async function ScheduleDataFetcher({ searchQuery }: { searchQuery?: string }) {
   const initialData = await getSchedules({
-    searchQuery: searchParams.search,
+    searchQuery,
     page: 1,
     pageSize: 20,
   });
