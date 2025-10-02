2bed0b4538f0fc79f4b0d4ef488d8adbdafaa796
kwngmin <kwngmink@gmail.com>
2025-10-02 11:19:50 +0900

    user-scalable-true

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  // maximumScale: 1,
+  // userScalable: false,
 };
 
 export default async function RootLayout({
