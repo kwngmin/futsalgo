05543bfcd36fbd828e35763fa3ae140d25a65273
kwngmin <kwngmink@gmail.com>
2025-10-24 16:12:23 +0900

    로그인, 새로운 팀 등록, 새로운 일정 추가 버튼 사이즈 조정

--- src/app/(main-layout)/schedules/ui/LoginButton.tsx
+++ src/app/(main-layout)/schedules/ui/LoginButton.tsx
@@ -7,14 +7,14 @@ const LoginButton = () => {
   return (
     <Link
       href="/login"
-      className="fixed bottom-28 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 sm:h-11 flex items-center justify-between bg-gray-900 text-white hover:bg-gray-800 rounded-full md:rounded-b-none md:rounded-t-2xl font-semibold z-20 px-3 active:bg-gray-700 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50 cursor-pointer"
+      className="fixed bottom-28 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 flex items-center justify-between bg-gray-900 text-white hover:bg-gray-800 rounded-full md:rounded-b-none md:rounded-t-2xl font-semibold z-20 px-3 active:bg-gray-700 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50 cursor-pointer"
       aria-label="로그인"
     >
-      <div className="flex items-center justify-center gap-3 sm:gap-2.5">
-        <div className="shrink-0 size-7 sm:size-6 flex items-center justify-center bg-white text-gray-700 rounded-full">
+      <div className="flex items-center justify-center gap-3">
+        <div className="shrink-0 size-7 flex items-center justify-center bg-white text-gray-700 rounded-full">
           <CircleUserRound className="size-5" strokeWidth={2.5} />
         </div>
-        <span className="md:text-sm font-semibold">로그인</span>
+        <span className="font-semibold">로그인</span>
       </div>
       <ChevronRight className="size-6 opacity-80" strokeWidth={1.5} />
     </Link>
--- src/app/(main-layout)/teams/ui/RegisterTeamButton.tsx
+++ src/app/(main-layout)/teams/ui/RegisterTeamButton.tsx
@@ -8,13 +8,13 @@ const RegisterTeamButton = () => {
   return (
     <Link
       href="/teams/create"
-      className="fixed bottom-28 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 sm:h-11 flex items-center justify-between bg-emerald-600 text-white hover:bg-emerald-800 rounded-full md:rounded-b-none md:rounded-t-2xl cursor-pointer font-semibold z-20 px-3 active:bg-emerald-900 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50"
+      className="fixed bottom-28 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 flex items-center justify-between bg-emerald-600 text-white hover:bg-emerald-800 rounded-full md:rounded-b-none md:rounded-t-2xl cursor-pointer font-semibold z-20 px-3 active:bg-emerald-900 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50"
     >
-      <div className="flex items-center justify-center gap-3 sm:gap-2">
-        <div className="shrink-0 size-7 sm:size-6 flex items-center justify-center bg-white text-emerald-700 rounded-full">
+      <div className="flex items-center justify-center gap-3">
+        <div className="shrink-0 size-7 flex items-center justify-center bg-white text-emerald-700 rounded-full">
           <Plus className="size-5" strokeWidth={2.5} />
         </div>
-        <span className="md:text-sm font-semibold">새로운 팀 등록</span>
+        <span className="font-semibold">새로운 팀 등록</span>
       </div>
       <ChevronRight className="size-6 opacity-80" strokeWidth={1.5} />
     </Link>
