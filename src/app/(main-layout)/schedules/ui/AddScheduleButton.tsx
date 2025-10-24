519521d1510acffc31a23caed53165dfac0696dc
kwngmin <kwngmink@gmail.com>
2025-10-24 16:59:11 +0900

    로그인, 새로운 팀 등록, 새로운 일정 추가 버튼 위치 조정

--- src/app/(main-layout)/schedules/ui/AddScheduleButton.tsx
+++ src/app/(main-layout)/schedules/ui/AddScheduleButton.tsx
@@ -8,7 +8,7 @@ const AddScheduleButton = () => {
   return (
     <Link
       href="/schedules/new"
-      className="fixed bottom-28 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 flex items-center justify-between bg-indigo-600 text-white hover:bg-indigo-800 rounded-full md:rounded-b-none md:rounded-t-2xl cursor-pointer font-semibold z-20 px-3 active:bg-indigo-900 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50"
+      className="fixed bottom-28 md:left-20 lg:left-64 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 flex items-center justify-between bg-indigo-600 text-white hover:bg-indigo-800 rounded-full md:rounded-b-none md:rounded-t-2xl cursor-pointer font-semibold z-20 px-3 active:bg-indigo-900 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50"
       aria-label="새로운 일정 추가"
     >
       <div className="flex items-center justify-center gap-3">
--- src/app/(main-layout)/schedules/ui/LoginButton.tsx
+++ src/app/(main-layout)/schedules/ui/LoginButton.tsx
@@ -7,7 +7,7 @@ const LoginButton = () => {
   return (
     <Link
       href="/login"
-      className="fixed bottom-28 md:left-20 lg:left-72 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 flex items-center justify-between bg-gray-900 text-white hover:bg-gray-800 rounded-full md:rounded-b-none md:rounded-t-2xl font-semibold z-20 px-3 active:bg-gray-700 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50 cursor-pointer"
+      className="fixed bottom-28 md:left-20 lg:left-64 md:bottom-0 left-0 right-0 sm:max-w-xs md:max-w-2xl mx-4 sm:mx-auto shrink-0 h-12 flex items-center justify-between bg-gray-900 text-white hover:bg-gray-800 rounded-full md:rounded-b-none md:rounded-t-2xl font-semibold z-20 px-3 active:bg-gray-700 active:scale-98 md:active:scale-100 transition-all shadow-md inset-shadow-sm inset-shadow-white/50 cursor-pointer"
       aria-label="로그인"
     >
       <div className="flex items-center justify-center gap-3">
